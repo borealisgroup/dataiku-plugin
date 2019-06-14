a703b3b1feccc08bf4b3cb3e5895c13ad1deea43
ziad.beyens@borealisgroup.com <ziad.beyens@borealisgroup.com>
2019-06-14 10:31:14 +0000

    Edited file 'custom-recipes/filter_correlation/recipe.py'

--- custom-recipes/filter_correlation/recipe.py
+++ custom-recipes/filter_correlation/recipe.py
@@ -20,7 +20,7 @@ from dataiku.customrecipe import *
 # Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
 
 # To  retrieve the datasets of an input role named 'input_A' as an array of dataset names:
-input_A_names = get_input_names_for_role('input_A_role')
+input_A_names = get_input_names_for_role('input')
 # The dataset objects themselves can then be created like this:
 input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
 
@@ -36,10 +36,10 @@ output_A_datasets = [dataiku.Dataset(name) for name in output_A_names]
 # user will be prompted for values.
 
 # The configuration is simply a map of parameters, and retrieving the value of one of them is simply:
-my_variable = get_recipe_config()['parameter_name']
+threshold = get_recipe_config()['threshold']
 
 # For optional parameters, you should provide a default value in case the parameter is not present:
-my_variable = get_recipe_config().get('parameter_name', None)
+# my_variable = get_recipe_config().get('parameter_name', None)
 
 # Note about typing:
 # The configuration of the recipe is passed through a JSON object
@@ -51,60 +51,23 @@ my_variable = get_recipe_config().get('parameter_name', None)
 # Your original recipe
 #############################
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
-# # Analyzing correlations between variables in merged_raw_data_targets_prepared
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
-# ### Correlations > 0.99
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 from eda.correlations import get_rejected_variables
 from wp8 import remove_volume_without_close
 
-# Read recipe inputs
-merged_raw_data_targets_prepared = dataiku.Dataset("merged_raw_data_targets_prepared")
-merged_raw_data_targets_prepared_df = merged_raw_data_targets_prepared.get_dataframe()
+# Read the input
+input_dataset = input_datasets[0]
+df = input_dataset.get_dataframe()
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-rejected_variables_99 = get_rejected_variables(merged_raw_data_targets_prepared_df, threshold=0.99)
+rejected_variables_99 = get_rejected_variables(df, threshold=0.99)
+df = df.drop(rejected_variables_99, axis=1)
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df = merged_raw_data_targets_prepared_df.drop(rejected_variables_99, axis=1)
+# df_without_volume = remove_volume_without_close(df)
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df.corr()
+# rejected_variables_90 = get_rejected_variables(df, threshold=0.90, substring_overrides='LCOc1')
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
-# For each RIC: CLOSE, HIGH, LOW and OPEN columns are highly correlated.
-# 
-# => Only the CLOSE will be kept for further analysis.
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
-# Do we remove the volume columns without?
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df_without_volume = remove_volume_without_close(df)
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
-# ### Correlations > 0.90
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-rejected_variables_90 = get_rejected_variables(df, threshold=0.90, substring_overrides='LCOc1')
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
-# High correlation between:
-# 
-# - US crude oil stocks | Brent Crude closing prices
-# - Baker Hughes RIG count - North America | Brent Crude closing prices
-# - global refinery runrates | global refinery outages
-# 
-# Which variable do we remove?
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Write recipe outputs
-data_targets_filter_high_corr = dataiku.Dataset("data_targets_filter_high_corr")
-data_targets_filter_high_corr.write_with_schema(df)
+# Write the output to the output dataset
+output_dataset =  output_datasets[0]
+output_dataset.write_with_schema(pd.DataFrame(output))
